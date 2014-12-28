1c5a8ea83504f04365ea7ca51331bd8d61d553be
Peihong Guo <peihongguo@gmail.com>
2014-12-28 01:56:37 -0600

    fixed bug in experimental code

--- experiment/driver.m
+++ experiment/driver.m
@@ -1,14 +1,49 @@
 close all;
 
-steps = [0.25, 0.025, 0.0025, 0.00025];
+testCase = 3;
+
+switch testCase
+    case 1
+        steps = [0.25, 0.025, 0.0025, 0.00025];
+    case 2
+        steps = [3, 1, 0.5];
+    case 3
+        steps = [3, 1, 0.5];
+    case 4
+        steps = [0.11, 0.055, 0.011];
+end
+
 terror = zeros(length(steps), 1);
 c = 0;
 for sid = 1:length(steps)
     step = steps(sid);
-    y0 = [-1:step:1]';
-    x0 = asin(y0);
-    h = @(x) sin(x);
-    ystar = [-1:0.05:1]';
+    
+    switch testCase
+        case 1
+            h = @(x) sin(x);
+            g = @(x) asin(x);
+            y0 = [-1:step:1]';
+            x0 = g(y0);
+            ystar = [-1:0.05:1]';
+        case 2
+            h = @(x) exp(x);
+            g = @(x) log(x);    
+            y0 = [1:step:28]';
+            x0 = g(y0);
+            ystar = [1:0.5:28]';
+        case 3
+            h = @(x) x.^3;
+            g = @(x) nthroot(x, 3);
+            y0 = [-27:step:27]';
+            x0 = g(y0);
+            ystar = [-27:0.5:27]';
+        case 4
+            h = @(x) erf(x);
+            g = @(x) erfinv(x);
+            y0 = [-0.99:step:0.99]';
+            x0 = g(y0);
+            ystar = [-0.99:0.03:0.99]';
+    end
     
     n = 10;
     
@@ -20,31 +55,34 @@ for sid = 1:length(steps)
     for k=2:n
         dx = x0 - x{k-1};
         phi{k-1} = h(x{k-1});
-        dphi = phi{k-1} - y0;
+        dphi = y0 - phi{k-1};
         R{k-1} = (dphi'*dphi)\(dphi'*dx);
-        b{k-1} = mean(dx) - R{k-1} * mean(y0);
+        b{k-1} = mean(dx) - R{k-1} * mean(dphi);
         x{k} = x{k-1} + R{k-1} * dphi;
         error(k) = norm(x{k} - x0);
     end
     
-%     figure; hold on;
-%     plot(x0, y0, '-x');
-%     plot(x{n}, y0, '-o');
-%     
-%     figure; plot(error);
+    figure; hold on;
+    plot(x0, y0, '-gx');
+    plot(x{n}, y0, '-ro');
+    title('training');
+    
+    figure; plot(error);
+    title('error');
     
     % test it
     xstar = cell(n, 1);
     xstar{1} = repmat(c, length(ystar), 1);
     for k=2:n
-        xstar{k} = xstar{k-1} + R{k-1} * (h(xstar{k-1}) - ystar) + b{k-1};
+        xstar{k} = xstar{k-1} + R{k-1} * (ystar - h(xstar{k-1})) + b{k-1};
     end
-    diffx = xstar{n} - asin(ystar);
+    diffx = xstar{n} - g(ystar);
     diffx = diffx(2:end-1);
     terror(sid) = sqrt((diffx'*diffx)/length(xstar))
     figure; hold on;
-    plot(asin(ystar), ystar, '-x');
-    plot(xstar{n}, ystar, '-o');
+    plot(g(ystar), ystar, '-gx');
+    plot(xstar{n}, ystar, '-ro');
+    title('test');
 end
 
 figure; plot(log(steps), terror, '-x');
